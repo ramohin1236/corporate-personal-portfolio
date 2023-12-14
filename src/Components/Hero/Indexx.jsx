c835255da6f31b163e8853d11ee911cf6bea17c7
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2023-12-14 07:14:13 -0800

    hero section added

--- src/Components/Hero/Indexx.jsx
+++ src/Components/Hero/Indexx.jsx
@@ -1,8 +1,41 @@
+import { Bio } from "../../../public/data";
+import { HeroBg, HeroContainer, HeroInnerContainer, HeroLeftContainer, HeroRightContainer, Img, ResumeButton, Span, SubTitle, TextLoop, Title } from "./HeroStyle";
+import Typewriter from 'typewriter-effect';
+
 
 const Hero = () => {
     return (
-        <div>
-            <p>Hero</p>
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    {/* <HeroBgAnimation /> */}
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src='https://i.ibb.co/0GgjNTb/IMG-20210903-WA0032.jpg'alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
         </div>
     );
 };
